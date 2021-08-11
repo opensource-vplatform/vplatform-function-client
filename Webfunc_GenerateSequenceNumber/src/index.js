cf2a38a9cbcbd5b69ca6846149db62b326d97ed2
MaggieYear <380075669@qq.com>
2021-08-11 17:52:10 +0800

    Webfunc_GenerateSequenceNumber

--- Webfunc_GenerateSequenceNumber/src/index.js
+++ Webfunc_GenerateSequenceNumber/src/index.js
@@ -2,23 +2,44 @@
  *	流水号函数
  */
 vds.import("vds.object.*");
-var main = function(param) {
-    var args = param.getArgs(),
-        argsLen = args ? args.length : 0,
-        key = argsLen >= 1 ? args[0] : null,
-        model = argsLen >= 2 ? args[1] : null,
-        sn = argsLen >= 3 ? args[2] : null
+vds.import("vds.exception.*");
+vds.import("vds.rpc.*");
+var main = function(key, model, sn) {
 
     if (vds.object.isUndefOrNull(key) || vds.object.isUndefOrNull(model))
-        throw new Error("传入参数不能为空，请检查");
+        throw vds.exception.newConfigException("传入参数不能为空，请检查");
 
-    try {
-        var scope = scopeManager.getWindowScope(),
-        windowCode = scope ? scope.getWindowCode() : "";
+    if (vds.object.isUndefOrNull(sn))
+        sn = null;
         
-        return executeExpression(windowCode, key, model, sn);
+    try {        
+        return executeExpression(key, model, sn);
     } catch (e) {
         throw e;
     }
+   
 }
-export{    main}
+
+var executeExpression = function( key, model, sn) {
+    var expression = "GenerateSequenceNumber(\"" + key + "\",\"" + model + "\",\"" + sn + "\")",
+        result = null;
+
+    vds.rpc.callCommandSync("WebExecuteFormulaExpression", null, {
+        "isOperation": true,
+        "operationParam": {
+            "expression": expression
+        },
+        "success": function (rs) {
+            result = rs.data.result;
+        },
+        "fail": function(e) {
+            throw e;
+        }
+    });
+
+    return result;
+}
+
+export{    
+    main
+}
