b26f3314c4b5bc438e6e2c4519d34c6c8834dc3a
MaggieYear <380075669@qq.com>
2021-08-11 18:00:58 +0800

    Webfunc_GetCookie

--- Webfunc_GetCookie/src/index.js
+++ Webfunc_GetCookie/src/index.js
@@ -1,19 +1,15 @@
 /**
  * 获取cookie值(从客户端获取cookie值)
  */
-vds.import("vds.object.*");
-var main = function(param) {
-    var args = param.getArgs(),
-        argsLen = args ? args.length : 0,
-        name = argsLen >= 1 ? args[0] : null,
-        defaultVal = argsLen >= 2 ? args[1] : null;
+vds.import("vds.object.*", "vds.exception.*", "vds.cookie.*");
+var main = function(name, defaultVal) {
 
     if (vds.object.isUndefOrNull(name))
-        throw new Error("传入cookie名称为空，请检查");
+        throw vds.exception.newConfigException("传入cookie名称为空，请检查");
     if (vds.object.isUndefOrNull(defaultVal))
-        throw new Error("传入cookie值的默认值为空，请检查");
+        throw vds.exception.newConfigException("传入cookie值的默认值为空，请检查");
 
-    var cookieVal = cookieUtil.vcookie({
+    var cookieVal = vds.cookie.get({
         "name": name
     });
 
