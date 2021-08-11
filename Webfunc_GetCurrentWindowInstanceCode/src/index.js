234310872ca27c3a3be87922eeda7f36173ceb5d
MaggieYear <380075669@qq.com>
2021-08-11 18:02:17 +0800

    Webfunc_GetCurrentWindowInstanceCode

--- Webfunc_GetCurrentWindowInstanceCode/src/index.js
+++ Webfunc_GetCurrentWindowInstanceCode/src/index.js
@@ -2,13 +2,12 @@
  * 
  * 
  */
+vds.import("vds.window.*");
 var main = function(param) {
 //		var currentWindowInstanceCode = scopeManager.getCurrentScopeId();//客户端方法里面执行会取到构件域
 //		return currentWindowInstanceCode ? currentWindowInstanceCode : null;
-    var winScope = scopeManager.getWindowScope();
-    if(winScope){
-        return winScope.getInstanceId();
-    }
-    return null;
+
+    return vds.window.getInstanceId();
+
 }
 export{    main}
